b80b1fef306107fee9a03e93d2e25c5c3f7bf6c8
byadiganteng-blip <byadiganteng@gmail.com>
2026-09-11 13:04:20 +0700

    Add content detector

--- content_detector.py
+++ content_detector.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
 class ContentDetector:
-    KEYWORDS = {
-        'anime': {'strong': ['anime', 'otaku', 'wibu'], 'characters': ['naruto', 'luffy', 'gojo'], 'titles': ['one piece', 'naruto']},
-        'manhwa': {'strong': ['manhwa', 'webtoon'], 'characters': ['sung jinwoo'], 'titles': ['solo leveling', 'orv']},
-        'manga': {'strong': ['manga']},
-        'gaming': {'strong': ['gameplay', 'highlight', 'gaming']},
-        'mlbb': {'strong': ['mlbb', 'mobile legends']},
-        'freefire': {'strong': ['free fire', 'booyah']},
-        'genshin': {'strong': ['genshin']},
-        'topup': {'strong': ['top up', 'topup', 'diamond murah']},
+    KW = {
+        'anime': {'s': ['anime','otaku'], 'c': ['naruto','luffy','gojo'], 't': ['one piece']},
+        'manhwa': {'s': ['manhwa','webtoon'], 't': ['solo leveling']},
+        'manga': {'s': ['manga']}, 'donghua': {'s': ['donghua']},
+        'mlbb': {'s': ['mlbb']}, 'freefire': {'s': ['free fire']},
+        'gaming': {'s': ['gameplay','highlight']},
     }
     def detect_from_filename(self, filename):
         text = filename.lower()
-        for ch in ['_', '-', '.', '(', ')']: text = text.replace(ch, ' ')
         results = []
-        for cat, data in self.KEYWORDS.items():
-            score = 0
-            for kw in data.get('strong', []):
-                if kw in text: score += 5
-            for c in data.get('characters', []):
-                if c in text: score += 4
-            for t in data.get('titles', []):
-                if t in text: score += 3
+        for cat, d in self.KW.items():
+            score = sum(5 for k in d.get('s', []) if k in text)
+            score += sum(4 for c in d.get('c', []) if c in text)
+            score += sum(3 for t in d.get('t', []) if t in text)
             if score > 0: results.append((cat, score))
         results.sort(key=lambda x: x[1], reverse=True)
         return results
     def detect_multiple(self, filename, max_cat=3):
-        return [cat for cat, _ in self.detect_from_filename(filename)[:max_cat]]
+        return [c for c, _ in self.detect_from_filename(filename)[:max_cat]]
