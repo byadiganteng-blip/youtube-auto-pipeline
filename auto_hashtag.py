8ea2e78148b9f0253d0d5642faf2ef60b3099b93
byadiganteng-blip <byadiganteng@gmail.com>
2026-09-11 12:28:36 +0700

    Add auto hashtag

--- auto_hashtag.py
+++ auto_hashtag.py
@@ -6,34 +6,29 @@ class AutoHashtag:
     def __init__(self):
         self.detector = ContentDetector()
         self.hashtags = {
-            'viral': ["#FYP", "#Viral", "#Trending", "#Shorts", "#YouTubeShorts", "#ViralIndonesia"],
-            'anime': ["#Anime", "#AnimeViral", "#AnimeEdit", "#Otaku", "#Wibu"],
-            'manhwa': ["#Manhwa", "#Webtoon", "#SoloLeveling", "#TBATE"],
-            'manga': ["#Manga", "#MangaViral"],
-            'donghua': ["#Donghua", "#SoulLand", "#BTTH"],
+            'viral': ["#FYP", "#Viral", "#Trending", "#Shorts"],
+            'anime': ["#Anime", "#AnimeViral", "#Otaku", "#Wibu"],
+            'manhwa': ["#Manhwa", "#Webtoon", "#SoloLeveling"],
+            'manga': ["#Manga"],
             'gaming': ["#Gaming", "#Gamer", "#GamingIndonesia"],
-            'mlbb': ["#MLBB", "#MobileLegends", "#DiamondMLBB"],
-            'freefire': ["#FreeFire", "#FFIndonesia", "#FreeFireBooyah"],
-            'engagement': ["#Subscribe", "#Like", "#Comment", "#Share"],
-            'indonesia': ["#Indonesia", "#GamersID", "#KreatorIndonesia"],
-            'reels': ["#Reels", "#ReelsVideo", "#ReelsIndonesia", "#ReelsFYP"],
+            'mlbb': ["#MLBB", "#MobileLegends"],
+            'freefire': ["#FreeFire", "#FFIndonesia"],
+            'topup': ["#TopUpGame", "#TopUpMurah"],
+            'engagement': ["#Subscribe", "#Like", "#Comment"],
+            'indonesia': ["#Indonesia", "#GamersID"],
+            'reels': ["#Reels", "#ReelsVideo", "#ReelsFYP"],
         }
-        self.branded = ["#YadStore", "#YadGaming"]
+        self.branded = ["#YadStore"]
 
     def generate(self, filename, title="", count=15, upload_type="video"):
         categories = self.detector.detect_multiple(filename + " " + title, max_cat=3)
         if not categories: categories = ['gaming']
         tags = list(random.sample(self.hashtags['viral'], 4))
-        if upload_type == "reels":
-            tags.extend(random.sample(self.hashtags['reels'], 3))
+        if upload_type == "reels": tags.extend(random.sample(self.hashtags['reels'], 3))
         primary = categories[0]
         if primary in self.hashtags:
             avail = [t for t in self.hashtags[primary] if t not in tags]
             tags.extend(random.sample(avail, min(7, len(avail))))
-        for cat in categories[1:]:
-            if cat in self.hashtags and len(tags) < 14:
-                avail = [t for t in self.hashtags[cat] if t not in tags]
-                if avail: tags.extend(random.sample(avail, min(3, len(avail))))
         tags.extend(random.sample(self.hashtags['engagement'], 2))
         tags.extend(random.sample(self.hashtags['indonesia'], 2))
         tags.append(random.choice(self.branded))
@@ -41,10 +36,8 @@ class AutoHashtag:
         return ' '.join(tags[:count])
 
     def _extract_part_number(self, filename):
-        patterns = ['part[-_ ]*([0-9]+)', 'episode[-_ ]*([0-9]+)', 'ep[-_ ]*([0-9]+)', '_([0-9]+)[.]mp4$']
-        text = filename.lower()
-        for p in patterns:
-            m = re.search(p, text)
+        for p in ['part[-_ ]*([0-9]+)', '_([0-9]+)[.]mp4$']:
+            m = re.search(p, filename.lower())
             if m:
                 try: return int(m.group(1))
                 except: pass
@@ -52,22 +45,10 @@ class AutoHashtag:
 
     def generate_title(self, filename, upload_type="video"):
         part_num = self._extract_part_number(filename)
-        cats = self.detector.detect_multiple(filename, max_cat=1)
-        cat = cats[0] if cats else 'gaming'
-        titles = {
-            'anime': ["Anime Edit Viral!", "Momen Epic Anime!"],
-            'manhwa': ["Manhwa Recommendation!", "Manhwa Panel Epic!"],
-            'gaming': ["Gameplay Epic!", "Gaming Highlight Terbaik!"],
-        }
-        base = random.choice(titles.get(cat, titles['gaming']))
         prefix = "#Shorts " if upload_type == "reels" else ""
-        if part_num: return prefix + "PART " + str(part_num) + " - " + base
-        return prefix + "PART 1 - " + base
+        base = random.choice(["Gameplay Epic!", "Viral Moment!", "Highlight Terbaik!"])
+        return (prefix + "PART " + str(part_num or 1) + " - " + base)
 
     def generate_description(self, filename, title, hashtags, upload_type="video"):
         part_num = self._extract_part_number(filename)
-        desc = title + "\n\n"
-        if part_num: desc += "PART " + str(part_num) + "\n\n"
-        desc += "Format: " + ("Reels" if upload_type == "reels" else "Video") + "\n\n"
-        desc += "Subscribe!\nLike & Comment!\n\nHASHTAG:\n" + hashtags + "\n\nCreated By Yad\n"
-        return desc
+        return title + "\n\nPART " + str(part_num or 1) + "\n\nSubscribe!\n\n" + hashtags + "\n\nCreated By Yad"
